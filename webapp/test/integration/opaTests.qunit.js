945af7cfe50995be7c8fe041cdeb3dc39b100c93
Sai Sreelekha SURAPARAJU <sreelekhas.in@mouritech.com>
2024-02-20 14:22:52 +0000

    Initial Version

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/departmentapp/departmentapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comdepartmentapp/departmentapp/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
